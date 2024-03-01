6aa695a816281361e71046578d474b955b2d84b3
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-03-01 15:20:46 +0100

    add map ville, route

--- src/Graph.java
+++ src/Graph.java
@@ -1,9 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Graph {
+  private HashMap<Ville, Set<Route>> routes;
     public Graph(File cities, File roads) {
+      routes = new HashMap<>();
       Scanner citiesScanner;
       Scanner roadsScanner;
       try {
@@ -19,15 +24,24 @@ public class Graph {
         double lng = Double.parseDouble(city[2]);
         double lat = Double.parseDouble(city[3]);
         Ville ville = new Ville(id, nom, lng, lat);
-        System.out.println(ville);
+        routes.put(ville, new HashSet<>());
       }
       while(roadsScanner.hasNextLine()) {
         String[] road = roadsScanner.nextLine().split(",");
         int idVilleDepart = Integer.parseInt(road[0]);
         int idVilleArrivee = Integer.parseInt(road[1]);
-        Route route = new Route(idVilleDepart, idVilleArrivee);
-        System.out.println(route);
+        Route r = new Route(idVilleDepart, idVilleArrivee);
+        Ville cmpVilleDepart = new Ville(idVilleDepart);
+        Set<Route> s = routes.get(cmpVilleDepart);
+        s.add(r);
+        routes.put(cmpVilleDepart, s);
+
+        Ville cmpVilleArrivee = new Ville(idVilleArrivee);
+        Set<Route> s1 = routes.get(cmpVilleArrivee);
+        s1.add(r);
+        routes.put(cmpVilleArrivee, s1);
       }
+      System.out.println(routes);
     }
 
     public void calculerItineraireMinimisantNombreRoutes(String villeDepart, String villeArrivee){
--- src/Ville.java
+++ src/Ville.java
@@ -1,8 +1,14 @@
+import java.util.Objects;
+
 public class Ville {
   private int id;
   private String nom;
   private double lng;
   private double lat;
+
+  public Ville(int id) {
+    this.id = id;
+  }
 
   public Ville(int id, String nom, double lng, double lat) {
     this.id = id;
@@ -19,5 +25,22 @@ public class Ville {
         ", lng=" + lng +
         ", lat=" + lat +
         '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Ville ville = (Ville) o;
+    return id == ville.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
   }
 }
